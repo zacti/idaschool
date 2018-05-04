02289c9ea0b54976d2bba60d31b69f6ce4731cd1
zacti <ll8317727@gmail.com>
2018-05-04 15:05:08 +0800

    fix heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root"home#index"
+  root '/' => "home#index"
   get '/school/work' => 'school#work'
   get '/school/hire' => 'school#hire'
   get '/school/graphic' => 'school#graphic'
